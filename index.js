63e790365674aa567e4882c9aeebcb5ca89aae03
AndersonJimenez <anjimhol23@gmail.com>
2023-08-13 03:20:41 -0500

    se realiza filtro de fecha

--- index.js
+++ index.js
@@ -17,7 +17,6 @@ const main = document.querySelector(".container-general_cards");
 
 const respuesta = await requestingHotels();
 const data = await respuesta.json();
-console.log(data);
 
 function loadInformation(data) {
   data.forEach((hotel) => {
@@ -73,41 +72,28 @@ function loadInformation(data) {
 loadInformation(data);
 // FILTER COUNTRIES
 const filterCountries = document.getElementById("filter-countries");
-console.log(filterCountries);
 filterCountries.addEventListener("change", () => {
   let valorOption = filterCountries.value;
-  console.log(valorOption);
   let optionSelect = filterCountries.options[filterCountries.selectedIndex];
-  console.log(optionSelect);
   const textSelected = optionSelect.textContent;
-  console.log(textSelected);
   let filteredData = data;
   if (valorOption != "all") {
     filteredData = data.filter((hotel) => hotel.country == textSelected);
-    console.log(filteredData);
   }
   main.innerHTML = "";
   loadInformation(filteredData);
 });
-// FILTER DATE
 
 // FILTER PRICES
 const filterPrices = document.getElementById("filter-prices");
-console.log(filterPrices);
 filterPrices.addEventListener("change", () => {
   let priceOptions = filterPrices.value;
-  console.log(priceOptions);
   let priceSelect = filterPrices.options[filterPrices.selectedIndex];
-  console.log(priceSelect);
   const textPreiceSelected = priceSelect.textContent;
-  console.log(textPreiceSelected);
   const changeToNumber = symbolToNumber(textPreiceSelected);
-  console.log(changeToNumber);
   let filteredPriceData = data;
-  console.log(filteredPriceData);
   if (priceOptions != "All") {
     filteredPriceData = data.filter((hotel) => hotel.price == changeToNumber);
-    console.log(filteredPriceData);
   }
   main.innerHTML = "";
   loadInformation(filteredPriceData);
@@ -117,7 +103,6 @@ filterPrices.addEventListener("change", () => {
 const dateCheckIn = document.getElementById("checkIn");
 const dateCheckOut = document.getElementById("checkOut");
 const today = new Date();
-
 function zerodate(dateZero) {
   const converText = "" + dateZero;
   if (converText.length === 1) {
@@ -126,34 +111,29 @@ function zerodate(dateZero) {
     return dateZero;
   }
 }
-
 const day = today.getDate();
 const month = today.getMonth() + 1;
 const year = today.getFullYear();
-
 const dateCheckInHotels = year + "-" + zerodate(month) + "-" + zerodate(day);
 const dateCheckOutHotels =
   year + "-" + zerodate(month) + "-" + zerodate(day + 1);
-
 dateCheckIn.setAttribute("min", dateCheckInHotels);
 dateCheckOut.setAttribute("min", dateCheckOutHotels);
-
 dateCheckIn.addEventListener("change", () => {
   const parts = dateCheckIn.value.split("-");
-
   const year = parseInt(parts[0]);
   const month = parseInt(parts[1]);
   const day = parseInt(parts[2]);
-
   const finalDate = year + "-" + zerodate(month) + "-" + zerodate(day + 1);
   dateCheckOut.setAttribute("min", finalDate);
-
-  let optionCheckIn = new Date(dateCheckIn.value);
 });
-dateCheckOut.addEventListener("change", () => {
-  let optionCheckOut = new Date(dateCheckOut.value);
-});
-
+function isHotelAvailable(hotel, resultCheckIn, differenceInMilliseconds) {
+  return (
+  resultCheckIn >= hotel.availabilityFrom &&
+    differenceInMilliseconds <= hotel.availabilityTo
+  )   
+}
+let dateCheckOutSelected = false;
 function calculateDifferenceDays() {
   const currentDateIni = new Date();
   currentDateIni.setHours(0, 0, 0, 0);
@@ -161,45 +141,38 @@ function calculateDifferenceDays() {
   const optionCheckInIni = new Date(dateCheckIn.value + " 00:00:00");
   optionCheckInIni.setHours(0, 0, 0, 0);
   const optionCheckIn = optionCheckInIni.getTime();
+  if (!dateCheckOutSelected) {
+    return;
+  }
   const optionCheckOut = new Date(dateCheckOut.value);
-
   const resultCheckIn = optionCheckIn - currentDateMilliseconds;
-  console.log(resultCheckIn);
-
-  // Calcula la diferencia en milisegundos entre las dos fechas
   const millisecondsDate = optionCheckOut - optionCheckIn;
-
-  // Convierte milisegundos a días
   const millisecondsInADay = 24 * 60 * 60 * 1000; // 86,400,000
-
   const differenceInMilliseconds =
     Math.round(millisecondsDate / millisecondsInADay) * millisecondsInADay;
-  console.log("Booking in milliseconds:", differenceInMilliseconds);
-  
-  let filterCurrentDateIni = data;
-  if (resultCheckIn == 0 ) {
-    filterCurrentDateIni = data.filter((hotel) => hotel.availabilityFrom == resultCheckIn);
-    console.log(filterCurrentDateIni);
-  }
+  const hotelesDisponibles = data.filter((hotel) => {
+    return isHotelAvailable(hotel, resultCheckIn, differenceInMilliseconds);
+  });
+  console.log(hotelesDisponibles);
+  main.innerHTML = "";
+  loadInformation(hotelesDisponibles);
 }
 dateCheckIn.value = "";
 dateCheckOut.value = "";
-
 dateCheckIn.addEventListener("change", calculateDifferenceDays);
-dateCheckOut.addEventListener("change", calculateDifferenceDays);
-
-
+dateCheckOut.addEventListener("change", function () {
+  dateCheckOutSelected = true;
+  calculateDifferenceDays();
+});
 
 // DELETE FILTER BUTTON CLEAR
 const resetBtn = document.getElementById("filter");
-// console.log(resetBtn);
 resetBtn.addEventListener("click", () => {
-  // Restablecer el valor seleccionado del select a su opción predeterminada
   filterCountries.selectedIndex = 0;
-
   dateCheckIn.value = "";
   dateCheckOut.value = "";
-  // Mostrar todos los hoteles nuevamente
+  dateCheckOutSelected = false;
+  calculateDifferenceDays();
   main.innerHTML = "";
   loadInformation(data);
 });
